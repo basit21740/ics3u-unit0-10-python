1e7a804f7a84dfd2a9b8b8433d20d731086e0931
abdul <abdul.butt@mths.ca>
2021-11-11 17:13:00 +0000

    added addition functions

---
+++ addition.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Abdul Basit
+# Created on: Nov 2021
+# This program add the two numbers the user inputs
+
+
+
+def main():
+    # This program add the two numbers the user inputs
+    
+    # process
+    answer = 0.1 + 0.2
+    
+    # output
+    print("the sum is", answer)
+    print("")
+    print("Done.")
+    
+    
+if __name__ == "__main__":
+    main()
